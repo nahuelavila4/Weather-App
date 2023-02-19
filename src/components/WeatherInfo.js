e22f478e62e7413f8d129f39fedbd8d92d34248a
Nahuel Avila <avilanahuel99@gmail.com>
2023-02-19 01:12:13 -0300

    2.1 Responsive

--- src/components/WeatherInfo.js
+++ src/components/WeatherInfo.js
@@ -5,13 +5,13 @@ const WeatherInfo = ({ weather }) => {
     <div className="asdg">
       <div className="estado">
         <div className="contenedor-temp">
-          <div className="temp"><h2>{weather?.current.temp_c}°</h2></div>
+          <div className="temp"><h3>{weather?.current.temp_c}°</h3></div>
           <img
             src={`https:${weather?.current.condition.icon}`}
             alt="Clima icono" className="imagen-clima"
           />
         </div>
-        <div className="cond-text"><h2>{weather?.current.condition.text}</h2></div>
+        <div className="cond-text"><h3>{weather?.current.condition.text}</h3></div>
       </div>
       <div className="contenedor-asd">
         <div><h4>{weather?.location.name}</h4></div>
